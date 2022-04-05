077ecf280e994e29e261799d815c5d6dde990c27
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-04-05 18:21:16 +0430

    Developing

---
+++ NightOwl.Web/Areas/Admin/Controller/AdminsController.cs
@@ -0,0 +1,82 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using NightOwl.DataLayer.Context;
+
+namespace NightOwl.Web.Areas.Admin.Controller
+{
+    [Area("Admin")]
+    public class AdminsController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private NightOwlContext _context;
+        private INotyfService _notyfService;
+
+        public AdminsController(NightOwlContext context, INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        [Route("/Admin/ManageRequest/Details/{adminId}")]
+        public IActionResult AdminDetails(int adminId)
+        {
+            var adminRequestDetails = _context.Admins.Find(adminId);
+
+            if(adminRequestDetails==null)
+                return NotFound();
+
+            return View(adminRequestDetails);
+        }
+
+        [Route("/Admin/ManageRequest/Confirm/{adminId}")]
+        public IActionResult ConfirmAdminRequest(int adminId)
+        {
+            try
+            {
+                var adminRequestDetails = _context.Admins.Find(adminId);
+
+                if (adminRequestDetails == null)
+                    return NotFound();
+
+                adminRequestDetails.IsActive = true;
+                _context.SaveChanges();
+
+                _notyfService.Success("Request Confirmed !");
+
+                return Redirect("/Admin");
+
+            }
+            catch
+            {
+                _notyfService.Error("Failed To Accept Request !");
+                return Redirect("/Admin");
+            }
+            
+        }
+
+        [Route("/Admin/ManageRequest/Decline/{adminId}")]
+        public IActionResult DeclineAdminRequest(int adminId)
+        {
+            try
+            {
+                var adminRequestDetails = _context.Admins.Find(adminId);
+
+                if (adminRequestDetails == null)
+                    return NotFound();
+
+                _context.Admins.Remove(adminRequestDetails);
+                _context.SaveChanges();
+
+                _notyfService.Success("Request Removed Successfully !");
+
+                return Redirect("/Admin");
+
+            }
+            catch
+            {
+                _notyfService.Error("Failed To Remove Request !");
+                return Redirect("/Admin");
+            }
+
+        }
+    }
+}
--- NightOwl.Web/Areas/Admin/Controller/HomeController.cs
+++ NightOwl.Web/Areas/Admin/Controller/HomeController.cs
@@ -1,29 +1,30 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NightOwl.Core.Services.Interfaces;
+using NightOwl.DataLayer.Context;
 
 namespace NightOwl.Web.Areas.Admin.Controller
 {
-    [Authorize]
     [Area("Admin")]
     public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
         IReviewRepository _reviewRepository;
         IMovieRepository _movieRepository;
+        private NightOwlContext _context;
 
-        public HomeController(IReviewRepository reviewRepository, IMovieRepository movieRepository)
+        public HomeController(IReviewRepository reviewRepository, IMovieRepository movieRepository,NightOwlContext context)
         {
             _reviewRepository = reviewRepository;
             _movieRepository = movieRepository;
+            _context = context;
         }
-
         public IActionResult Index()
         {
             ViewBag.ReviewsNo = _reviewRepository.GetTotalReviews();
             ViewBag.MoviesNo = _movieRepository.GetTotalMovies();
             ViewBag.SeriesNo = _movieRepository.GetTotalSeries();
             ViewBag.ComingSoonNo = _movieRepository.GetTotalComingSoon();
-
+            ViewBag.AdminRequests = _context.Admins.Where(a => a.IsActive==false).ToList();
             ViewBag.UnPublishedReviews = _reviewRepository.GetUnPublishedReviews();
             return View();
         }
