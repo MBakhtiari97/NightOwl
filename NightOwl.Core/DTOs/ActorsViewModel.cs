b1b4e8850224ba7359f39516b071304bb86e4366
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-04-05 09:47:37 +0430

    Completed Actors Section

---
+++ NightOwl.Core/DTOs/ActorsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace NightOwl.Core.DTOs
+{
+    public class ActorsViewModel
+    {
+        public int ActorId { get; set; }
+        public string ActorName { get; set; }
+        public string ActorGender { get; set; }
+        public IFormFile? ActorImage { get; set; }
+    }
+}
---
+++ NightOwl.Web/Areas/Admin/Controller/ActorsController.cs
@@ -0,0 +1,92 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using NightOwl.Core.DTOs;
+using NightOwl.DataLayer.Context;
+using NightOwl.DataLayer.Entities;
+
+namespace NightOwl.Web.Areas.Admin.Controller
+{
+    [Area("Admin")]
+    public class ActorsController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        NightOwlContext _context;
+        private INotyfService _notyfService;
+
+        public ActorsController(NightOwlContext context,INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        [BindProperty] 
+        public ActorsViewModel Actors { get; set; }
+
+        [Route("/Admin/Actors")]
+        public IActionResult Index()
+        {
+            var actors = _context.Actors.ToList();
+            return View(actors);
+        }
+
+        [Route("/Admin/Actors/Details/{actorId}")]
+        public IActionResult ActorDetails(int actorId)
+        {
+            var actorsDetails = _context.Actors.Find(actorId);
+            return View(actorsDetails);
+        }
+
+        [Route("/Admin/Actors/New")]
+        public IActionResult CreateActor()
+        {
+            return View();
+        }
+
+        [Route("/Admin/Actors/New")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateActor([Bind("ActorId,ActorName,ActorGender,ActorImage")] Actors actor)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(actor);
+
+                if (Actors.ActorImage?.Length > 0)
+                {
+                    var actorImageName = actor.ActorName.Replace(" ", "").ToString()
+                                         + Path.GetExtension(Actors.ActorImage.FileName);
+                    actor.ActorImage = actorImageName;
+
+                    var actorImagePath = Path.Combine(Directory.GetCurrentDirectory(),
+                        "wwwroot",
+                        "img",
+                        "Actors",
+                        actorImageName);
+
+                    using (var stream = new FileStream(actorImagePath, FileMode.Create))
+                    {
+                        Actors.ActorImage.CopyTo(stream);
+                    }
+
+                }
+                else
+                {
+                    actor.ActorImage = "Default.jpg";
+                }
+
+                _context.Actors.Add(actor);
+                _context.SaveChanges();
+
+                _notyfService.Success($"{actor.ActorName} Has Been Added Successfully !");
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                _notyfService.Error("Cannot Add Actor ! Something Wrong Happened :(");
+                return View();
+            }
+            
+        }
+    }
+}
